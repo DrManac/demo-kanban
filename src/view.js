0bb0b41a79276d65d03f409e731b032e23e36925
DrManac <drploskikh@gmail.com>
2020-12-02 12:32:48 +0300

    Translated to JSX

--- src/view.js
+++ src/view.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-
-const e = React.createElement;
 
 class KanbanCard extends React.Component {
     render () {
-        return e(Draggable, {draggableId: this.props.card.id, index: this.props.index}, 
-            (provided) => e('div', {
-                className: 'card',
-                ref: provided.innerRef, 
-                ...provided.dragHandleProps, 
-                ...provided.draggableProps}, this.props.card.text),
-        )
+        return <Draggable draggableId={this.props.card.id} index={this.props.index}>
+            {
+                (provided) => <div className='card' ref={provided.innerRef} 
+                    {...provided.dragHandleProps} {...provided.draggableProps}>
+                        {this.props.card.text}
+                    </div>
+            }
+        </Draggable>
     }
 }
 
 class KanbanColumn extends React.Component {
     render () {
-        return e('div', {className: 'col'}, [
-            e('div', {className: 'col-header', key: -1}, this.props.name),
-            e(Droppable, {key: 1, droppableId: this.props.id}, 
-                (provided) =>  
-                    e('div', {}, e('div', {
-                        ref: provided.innerRef, 
-                        ...provided.droppableProps, 
-                        ...provided.droppablePlaceholder
-                    }, this.props.cards.map((c, i) => e(KanbanCard, {key: c.id, index: i, card: c}))),
-                    provided.placeholder)
-            )
-        ])
+        return <div className='col'>
+            <div className='col-header'>{this.props.column.name}</div>
+            <Droppable droppableId={this.props.column.id}>
+                {
+                    (provided) => <div>
+                        <div ref={provided.innerRef} {...provided.droppableProps} {...provided.droppablePlaceholder}>
+                            {
+                                this.props.column.cards.map((c, i) => <KanbanCard key={c.id} index={i} card={c}></KanbanCard>)
+                            }
+                        </div>
+                        {provided.placeholder}
+                    </div>
+                }
+            </Droppable>
+        </div>
     }
 }
 
@@ -43,15 +44,17 @@ export class Kanban extends React.Component {
         this.props.moveCard(result.source.droppableId, result.source.index, result.destination.droppableId, result.destination.index)
     }
     render () {
-        return e(DragDropContext, {onDragEnd: this.onDragEnd}, this.props.state.columns.map((c, i) => e(KanbanColumn, {...c, key: c.id})))
+        return <DragDropContext onDragEnd={this.onDragEnd}>
+            {
+                this.props.state.columns.map((c) => <KanbanColumn key={c.id} column={c}></KanbanColumn>)
+            }
+        </DragDropContext>
     }
   }
 
 export class Toolbar extends React.Component {
     render () {
-        return [
-            e('input', {type: "button", value: this.props.name, onClick: this.props.action, key: 1})
-        ]
+        return <input type='button' value={this.props.name} onClick={this.props.action}></input>
     }
 }
   
